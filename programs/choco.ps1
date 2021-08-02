251ec5c64ced50666ac9bf7972822222af94b67e
Erdem <rifaterdemsahin@gmail.com>
2021-08-02 15:20:53 +0100

    Update choco.ps1

--- programs/choco.ps1
+++ programs/choco.ps1
@@ -1,3 +1,19 @@
+cls
+$OutputVariable = (choco) | Out-String
+$OutputVariable  | Select-String -Pattern 'Chocolatey' -CaseSensitive -SimpleMatch
+if ($OutputVariable.length -gt 5) {
+    Write-Output "Chocolatey already installed"
+}
+else
+{
+    Read-Host -Prompt ' Run this before start : Set-ExecutionPolicy Bypass -Scope Process -Force; press any key to continue, ctrl+c to exit '
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+
+$User = Read-Host -Prompt ' Press any key to start the installation of the programs'
+echo ' ------------- Started to Install the programs ------------- '
 
 $list = New-Object Collections.Generic.List[String]
 $list.Add("audacity")
@@ -71,7 +87,11 @@ Foreach ($mypackage in $list)
 choco install $mypackage -y
 }
 
+echo ' ----------------- Installation Complete -------------- '
+echo ' Now login to the tools to be able to use them'
+
+choco upgrade all -y
 
 
 choco list --localonly
-todo verify install count
+#todo verify install count
