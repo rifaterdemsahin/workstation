d77d0b0b955177e826ccd0572c1efe452b8091f9
Erdem <rifaterdemsahin@gmail.com>
2021-08-02 15:16:53 +0100

    Update choco.ps1

--- programs/choco.ps1
+++ programs/choco.ps1
@@ -23,17 +23,7 @@ $list.Add("autohotkey.install")
 $list.Add("autohotkey")
 $list.Add("adobereader")
 $list.Add("7zip.install")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
-$list.Add("")
+$list.Add("microsoft-teams")
 $list.Add("obs-studio")
 $list.Add("notepadplusplus.install")
 $list.Add("nomachine")
@@ -45,40 +35,41 @@ $list.Add("jre8")
 $list.Add("jdk8")
 $list.Add("javaruntime")
 $list.Add("GoogleChrome")
-$list.Add("")
+$list.Add("obs-studio.install")
 $list.Add("greenshot")
+$list.Add("slack")
+$list.Add("skype")
+$list.Add("python3")
+$list.Add("python")
+$list.Add("procexp")
+$list.Add("PowerShell")
+$list.Add("putty.install")
+$list.Add("postman")
+$list.Add("paint.net")
+$list.Add("vscode")
+$list.Add("vscode.install")
+$list.Add("wox")
+$list.Add("vlc")
+$list.Add("visualstudio2019enterprise")
+$list.Add("visualstudio-installer")
+$list.Add("virtualbox")
+$list.Add("vim")
+$list.Add("vcredist2017")
+$list.Add("vcredist2015")
+$list.Add("vcredist140")
+$list.Add("telegram.install")
+$list.Add("telegram")
+$list.Add("streamlabs-obs")
+$list.Add("streamdeck")
+$list.Add("steam-client")
+$list.Add("sql-server-management-studio")
+$list.Add("sourcetree")
+$list.Add("audacity")
 
 todo add a loop here
-choco install audacity -y
+choco install  -y
 
-obs-studio.install 27.0.1
-paint.net 4.2.16
-postman 8.7.0
-PowerShell 5.1.14409.20180811
-procexp 16.42
-putty.install 0.76
-python 3.9.6
-python3 3.9.6
-skype 8.73.0.124
-slack 4.18.0
-sourcetree 3.4.1
-sql-server-management-studio 15.0.18386.0
-steam-client 2.10.91.91
-streamdeck 5.0.1.14252
-streamlabs-obs 1.3.1
-telegram 2.7.4
-telegram.install 2.7.4
-vcredist140 14.29.30037
-vcredist2015 14.0.24215.20170201
-vcredist2017 14.16.27033
-vim 8.2.3192
-virtualbox 6.1.24
-visualstudio-installer 2.0.1
-visualstudio2019enterprise 16.10.2.0
-vlc 3.0.16
-vscode 1.58.2
-vscode.install 1.58.2
-wox 1.3.524.20180714
+
 
 choco list --localonly
 todo verify install count
