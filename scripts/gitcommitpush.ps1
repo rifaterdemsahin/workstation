3b685e5d7c093d4c0d393764c7bd850e135a5c07
Erdem <info@pexabo.com>
2021-10-10 11:09:53 +0100

    basic add

---
+++ scripts/gitcommitpush.ps1
@@ -0,0 +1,4 @@
+cd "C:\Users\erdem\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\workstation"
+git add .
+git commit . -m 'basic add'
+git push
---
+++ scripts/starttabs.ps1
@@ -0,0 +1,28 @@
+
+start chrome https://www.xsplit.com/
+start chrome https://twitter.com/
+start chrome https://www.notion.so/
+start chrome https://mail.google.com/mail/u/0/#inbox
+start chrome https://calendar.google.com/calendar/u/0/r?tab=mc
+start chrome https://web.whatsapp.com/
+start chrome https://teams.microsoft.com/
+start chrome https://web.telegram.org/z/
+start chrome https://www.skillshare.com/classes/Creating-Your-First-CICD-Pipeline-With-Azure-DevOps/701578588
+start chrome https://www.visualstudio.com/Fusion/_workitems/recentlyupdated/
+start chrome https://portal.azure.com/#@pexabo.com/resource/subscriptions/ad744a08-2411-4d78-b417-1f1c83f4f741/providers/Microsoft.Support/supportTickets/2109270050002938
+start chrome https://dev.azure.com/
+
+
+#TODOS
+#fix the profiles issue
+#fix the multiscreen location issue
+
+#NOTES
+#Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe" '-P "Profile 1" -no-remote'
+#runas /user:administrator “C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe
+#start chrome https://www.xsplit.com/
+#chrome.exe --js-flags="--trace-opt --trace-deopt --trace-bailout"
+#Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" https://www.xsplit.com/
+#Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" '--profile-directory="Pexabo Info"'
+#start chrome https://www.xsplit.com/ --profile-directory=Default
+#https://peter.sh/experiments/chromium-command-line-switches/#load-extension
