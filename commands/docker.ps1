80748f81f8d24f254b4da403bc917d520ae06caf
Erdem <rifaterdemsahin@gmail.com>
2021-10-21 11:03:21 +0100

    Update docker.ps1

--- commands/docker.ps1
+++ commands/docker.ps1
@@ -1,2 +1,15 @@
 docker build --file dockerfile -t devopsrunner:1.0 .     
 docker image ls
+
+
+Read logs
+PS C:\Users\erdem\AppData\local\Docker> code log.txt
+PS C:\Users\erdem\AppData\local\Docker> pwd
+
+Cleanup
+
+Remove-Item 'C:\Users\erdem\AppData\local\Docker' -Recurse
+Remove-Item 'C:\Users\erdem\AppData\Roaming\Docker Desktop' -Recurse
+Remove-Item 'C:\Users\erdem\AppData\Roaming\Docker' -Recurse
+
+https://forums.docker.com/t/docker-failed-to-initialize-operation-timed-out/111321/2
