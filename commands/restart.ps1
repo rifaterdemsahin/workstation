e525c213cb6b88ac3c60581d9153ff0e9e616df9
Erdem <rifaterdemsahin@gmail.com>
2021-10-21 10:14:28 +0100

    Create restart.ps1

---
+++ commands/restart.ps1
@@ -0,0 +1,5 @@
+type shutdown, followed by the option you wish to execute. 
+To shut down your computer, type shutdown /s
+To restart your computer, type shutdown /r
+To log off your computer type shutdown /l
+For a complete list of options type shutdown /?
