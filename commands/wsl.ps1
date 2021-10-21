775c14e52c6d676c010d147e265b154de908ccb9
Erdem <rifaterdemsahin@gmail.com>
2021-10-21 10:51:52 +0100

    Update wsl.ps1

--- commands/wsl.ps1
+++ commands/wsl.ps1
@@ -1,3 +1,9 @@
  wsl --install
  'wsl --shutdown'.
  wsl.exe --set-default-version 2
+
+
+Hyper V
+HyperV did not enable on the dell xps it worked
+Enabled on the workstation
+https://twitter.com/rifaterdemsahin/status/1451123842580103172
