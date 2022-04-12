ba4417f189b70f5c19588aaaa49c9591a9f80ff4
Erdem <rifaterdemsahin@gmail.com>
2022-04-12 14:14:47 +0100

    Create wingetalternative.ps1

---
+++ programs/wingetalternative.ps1
@@ -0,0 +1,7 @@
+winget install --id Git.Git
+winget install --id Microsoft.VisualStudioCode
+winget install --id Docker.DockerDesktop
+winget install --id OpenJS.NodeJS.LTS
+winget install --id Python.Python.3
+winget install --id Microsoft.AzureCLI
+winget install --id Microsoft.VisualStudio.2019.Community
