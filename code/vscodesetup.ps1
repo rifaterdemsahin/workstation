83c29c0430ffa2844365d6c53594c45a9ef9f3de
Erdem <rifaterdemsahin@gmail.com>
2022-04-12 14:45:04 +0100

    Create vscodesetup.ps1

---
+++ code/vscodesetup.ps1
@@ -0,0 +1,6 @@
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.azurecli
+code --install-extension argutec.argutec-azure-repos
+code --install-extension ms-azuretools.vscode-azurestorage
+code --install-extension ms-azure-devops.azure-pipelines
