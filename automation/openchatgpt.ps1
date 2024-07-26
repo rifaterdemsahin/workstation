f9295ee25cab9b91a4478369753edf1de818e26d
Erdem <rifaterdemsahin@gmail.com>
2024-07-26 14:36:44 +0100

    Add files via upload

---
+++ automation/openchatgpt.ps1
@@ -0,0 +1,10 @@
+# Define the URL
+$url = "https://chatgpt.com/"
+
+# Define the path to Chrome
+$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+# Open three separate Chrome windows with the specified URL
+Start-Process -FilePath $chromePath -ArgumentList $url
+Start-Process -FilePath $chromePath -ArgumentList $url
+Start-Process -FilePath $chromePath -ArgumentList $url
